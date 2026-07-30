605c2416cf7316e6e9203411e1d30112543c0675
giovannaps <giovannapslima.gpsl@gmail.com>
2026-07-30 19:27:13 -0300

    feat: Adiciona código js

---
+++ parte1-automacao/script.js
@@ -0,0 +1,21 @@
+//Simulação de um nó code do n8n 
+
+var User = {
+    id: 1042,
+    username: "dev_candidate",
+    email: "candidato@email.com",
+    created_at: "2026-03-30T10:00:00Z"
+};
+
+function welcome(user) {
+    if (user.email.includes("@empresa.com")) {
+        user.welcome_message = ("Bem-vindo(a) ao time, @" + user.username + "!");
+    }
+    else {
+        user.welcome_message = ("Seja muito bem-vindo(a) à nossa comunidade, @" + user.username + "!");
+    }
+    return user;
+}
+
+welcome(User);
+console.log(welcome(User).welcome_message);
